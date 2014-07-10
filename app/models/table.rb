e0dfeed5f239edfa91b044c3ce4c5d1883b7fe9c
Joan Tiffany Siy <joan@teamcodeflux.com>
2014-07-10 11:20:09 +0800

    Added query methods in table to get seat counts

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,4 +1,12 @@
 class Table < ActiveRecord::Base
   belongs_to :event
   has_many :guests
+
+  def unassigned_seats
+    Table.joins("LEFT JOIN guests ON guests.table_id = tables.id").where("tables.id = ?", self.id).select("tables.max_seats - SUM(guests.assigned_seats) AS value").take.value ||= self.max_seats
+  end
+
+  def occupied_seats
+    Guest.select("SUM(guests.assigned_seats) AS value").where(:table => self, :registered => :t).take.value ||= 0
+  end
 end
